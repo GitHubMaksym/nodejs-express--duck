1f2c8320033330e1ec375251b6bfc99134fb0bda
Maksym <maksym.levenets@gmail.com>
2023-01-01 11:30:53 +0200

    Pages are added. Port is changed.

--- README.md
+++ README.md
@@ -7,4 +7,4 @@ NodeJS with Express Duck tales
 3. npm install express --save
 
 4. node server.js
-5. http://localhost:3000/
+5. http://127.0.0.1:7000/about
--- server.js
+++ server.js
@@ -1,11 +1,33 @@
-const express = require('express');
-const app = express();
-const port = 3000;
+const app = require('express')();
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+const host = '127.0.0.1';
+const port = 7000;
+
+app.get('/home', (req, res) => {
+    res.status(200).type('text/plain');
+    res.send('Home page');
 });
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+app.get('/about', (req, res) => {
+    res.status(200).type('text/plain');
+    res.send('About page');
+});
+
+app.post('/api/admin', (req, res) => {
+    res.status(200).type('text/plain');
+    res.send('Create admin request');
+});
+
+app.post('/api/user', (req, res) => {
+    res.status(200).type('text/plain');
+    res.send('Create user request');
+});
+
+app.use((req, res, next) => {
+    res.status(404).type('text/plain');
+    res.send('Not found');
+});
+
+app.listen(port, host, function () {
+    console.log(`Server listens http://${host}:${port}`);
 });
